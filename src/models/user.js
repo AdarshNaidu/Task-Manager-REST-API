bd48283a762d17cb21ddfb507bc7d973a684ba76
AdarshNaidu <naidu.adarsh2511@gmail.com>
2020-01-16 23:04:11 +0530

    deleting user deletes tasks

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -90,6 +91,13 @@ userSchema.statics.findByCredentials = async (email, password) => {
 
     return user;
 }
+
+//Delete uesr task when user is removed
+userSchema.pre('remove', async function(next){
+    const user = this;
+    await Task.deleteMany({ owner: user._id })
+    next();
+})
 
 // Hash the plain text password before saving
 userSchema.pre('save', async function(next){
